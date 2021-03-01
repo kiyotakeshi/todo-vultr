e5ab652c7262b20d7121369de3cf456458e45d37
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-02 03:21:07 +0900

    init commit

---
+++ config.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "~> 0.14.0"
+
+  // create bucket in advance
+  // @see README.md
+//  backend "s3" {
+//    bucket = "kiyotake-todo-vultr-terraform"
+//    key = "terraform.tfstate"
+//    region = "ap-northeast-1"
+//  }
+}
+
